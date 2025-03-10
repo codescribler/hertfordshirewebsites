3953c121e9c03d30cabd91dfb42a3622d2003c39
Daniel <daniel@dreamfree.co.uk>
2025-03-10 12:54:57 +0000

    Update British English spelling (optimisation vs optimization) for service pages and URLs

--- git-commands.bat
+++ git-commands.bat
@@ -1,5 +1,7 @@
 @echo off
 cd hertfordshirewebsites
 git status
+git add .
+git commit -m "Latest build and updates to master branch"
 git push origin master
 echo Changes successfully pushed to GitHub repository
